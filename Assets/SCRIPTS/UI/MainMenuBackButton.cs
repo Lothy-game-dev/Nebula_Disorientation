49002e08fd20e623304aefa0c6b9e0b82ba1f6d0
Kaann2507 <khangdzvcl123@outlook.com>
2023-09-05 16:10:45 +0700

    Update 5/9

---
+++ Assets/SCRIPTS/UI/MainMenuBackButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBackButton : MonoBehaviour
+{
+    #region ComponentVariables
+    // Variables used for calling componenets attached to the game object only
+    // Can be public or private
+    #endregion
+    #region InitializeVariables
+    // Variables that will be initialize in Unity Design, will not initialize these variables in Start function
+    // Must be public
+    // All importants number related to how a game object behave will be declared in this part
+    public GameObject BackScene;
+    #endregion
+    #region NormalVariables
+    // All other variables apart from the two aforementioned types
+    // Can be public or private, prioritize private if possible
+    #endregion
+    #region Start & Update
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Collider2D>() == null)
+        {
+            gameObject.AddComponent<BoxCollider2D>();
+        }
+        // Initialize variables
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call function and timer only if possible
+    }
+    #endregion
+    #region Function group 1
+    // Group all function that serve the same algorithm
+    private void OnMouseDown()
+    {
+        if (BackScene != null && BackScene.name != "MainMenuScene")
+        {
+            FindObjectOfType<MainMenuCameraController>().ChangeToScene(BackScene);
+        } else
+        {
+            FindObjectOfType<MainMenuCameraController>().BackToMainMenu();
+        }
+    }
+    #endregion
+    #region Function group ...
+    // Group all function that serve the same algorithm
+    #endregion
+}
