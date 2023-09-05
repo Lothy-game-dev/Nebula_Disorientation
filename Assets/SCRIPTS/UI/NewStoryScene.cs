706228f7d40a8bf966b146045c36e6d59b9ae477
Lothy-game-dev <thestudyhuy@gmail.com>
2023-09-05 20:25:04 +0700

    New Story Done UI

--- Assets/SCRIPTS/UI/NewStoryScene.cs
+++ Assets/SCRIPTS/UI/NewStoryScene.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class NewStoryScene : MonoBehaviour
+public class NewStoryScene : MainMenuSceneShared
 {
     #region ComponentVariables
     // Variables used for calling componenets attached to the game object only
     // Can be public or private
     #endregion
     #region InitializeVariables
-    public InputField NameField;
+    public TMP_InputField NameField;
+    public GameObject[] StartAnimOn;
+    public GameObject[] StartAnimOff;
+    public GameObject Contract;
     #endregion
     #region NormalVariables
     private bool startWriting;
@@ -29,11 +33,39 @@ public class NewStoryScene : MonoBehaviour
         // Call function and timer only if possible
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            NameField.ActivateInputField();
+            if (!startWriting)
+            {
+                NameField.ActivateInputField();
+                startWriting = true;
+            }
+        }
+        if (!NameField.isFocused)
+        {
+            NameField.DeactivateInputField();
+            startWriting = false;
         }
     }
     #endregion
     #region Function group 1
     // Group all function that serve the same algorithm
+    public override void StartAnimation()
+    {
+        foreach (var go2 in StartAnimOff)
+        {
+            go2.SetActive(false);
+        }
+        PilotContract pc = Contract.GetComponent<PilotContract>();
+        if (pc!=null)
+        {
+            pc.EULAList[pc.currentPages].SetActive(false);
+            pc.currentPages = 0;
+            pc.EULAList[pc.currentPages].SetActive(true);
+        }
+        foreach (var go in StartAnimOn)
+        {
+            go.SetActive(true);
+        }
+        NameField.text = "";
+    }
     #endregion
 }
