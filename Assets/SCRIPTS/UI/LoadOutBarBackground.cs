c7aa2e9c8daf06697712b1e1a355838caebeb14b
Lothy-game-dev <thestudyhuy@gmail.com>
2023-09-11 22:34:41 +0700

    Load out Weapon Box

---
+++ Assets/SCRIPTS/UI/LoadOutBarBackground.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOutBarBackground : MonoBehaviour
+{
+    #region ComponentVariables
+    // Variables used for calling componenets attached to the game object only
+    // Can be public or private
+    #endregion
+    #region InitializeVariables
+    public GameObject Bar;
+    #endregion
+    #region NormalVariables
+    public int DisableCollider;
+    #endregion
+    #region Start & Update
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize variables
+        DisableCollider = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call function and timer only if possible
+    }
+    private void LateUpdate()
+    {
+        if (DisableCollider == 0)
+        {
+            GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            GetComponent<Collider2D>().enabled = true;
+        }
+        DisableCollider = 1;
+    }
+    #endregion
+    #region Mouse Check
+    private void OnMouseDown()
+    {
+        if (Bar.GetComponent<LoadOutBar>()!=null)
+        {
+            Bar.GetComponent<LoadOutBar>().BackgroundMouseDown();
+        }
+    }
+    #endregion
+}
