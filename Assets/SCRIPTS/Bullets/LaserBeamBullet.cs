8b76b691a6abf5aa2eaed69a24ca080c41503746
Kaann2507 <khangdzvcl123@outlook.com>
2023-10-07 14:08:08 +0700

    push

---
+++ Assets/SCRIPTS/Bullets/LaserBeamBullet.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamBullet : BulletShared
+{
+    #region ComponentVariables
+    // Variables used for calling componenets attached to the game object only
+    // Can be public or private
+    #endregion
+    #region InitializeVariables
+    // Variables that will be initialize in Unity Design, will not initialize these variables in Start function
+    // Must be public
+    // All importants number related to how a game object behave will be declared in this part
+    #endregion
+    #region NormalVariables
+    // All other variables apart from the two aforementioned types
+    // Can be public or private, prioritize private if possible
+    #endregion
+    #region Start & Update
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize variables
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call function and timer only if possible
+    }
+    #endregion
+    #region Function group 1
+    // Group all function that serve the same algorithm
+    #endregion
+    #region Function group ...
+    // Group all function that serve the same algorithm
+    #endregion
+}
