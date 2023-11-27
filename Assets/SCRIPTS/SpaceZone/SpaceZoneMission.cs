bb7f03219fab5a8104218ddc213fcaa6bdeb29cd
Kaann2507 <khangdzvcl123@outlook.com>
2023-11-27 15:08:25 +0700

    p

--- Assets/SCRIPTS/SpaceZone/SpaceZoneMission.cs
+++ Assets/SCRIPTS/SpaceZone/SpaceZoneMission.cs
@@ -261,8 +261,6 @@ public class SpaceZoneMission : MonoBehaviour
         stat.StageName = MissionStageName;
         Timer.DoneSetupTimer = false;
         stat.UpdateStatistic();
-        stat.UpdateSessionPlaytime();
-        stat.isStart = true;
         TextGO.CreateText("Mission Accomplished!", Color.green);
         
     }
